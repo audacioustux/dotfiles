a8e62247f66b3130ea212dcfe7d2cca50678368f
Tanjim Hossain <tangimhossain1@gmail.com>
2021-07-19 19:49:52 +0600

    add p10k

--- init.zsh
+++ init.zsh
@@ -3,6 +3,7 @@ ln -i .zshrc ~/
 ln -i .tmux.conf ~/
 ln -i .alacritty.yml ~/
 ln -i .tool-versions ~/
+ln -i .p10k.zsh ~/
 
 ln -i .config/nvim/init.vim ~/.config/nvim/
 ln -i .config/nvim/coc-settings.json ~/.config/nvim/
