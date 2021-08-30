16b9e84cd465f8ceb6f6bf7e378edd84d082feed
Tanjim Hossain <tangimhossain1@gmail.com>
2021-08-30 22:13:40 +0600

    wubba lubba dub dub

--- .aliases.zsh
+++ .aliases.zsh
@@ -1,3 +1,4 @@
 alias vim="nvim"
 alias t="env TERM=screen-256color tmux new-session -A -s main"
 alias tunnelIpfs="ssh -f -p 22 -L 4001:localhost:4001 -L 5001:localhost:5001 -L 8080:localhost:8080 audacioustux@devlap -N"
+
