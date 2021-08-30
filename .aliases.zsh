a7bd2665d8b80a68631ef0414160984a7a6f2338
Tanjim Hossain <tangimhossain1@gmail.com>
2021-08-30 20:19:18 +0600

    update

--- .aliases.zsh
+++ .aliases.zsh
@@ -1,2 +1,3 @@
 alias vim="nvim"
-alias t="tmux new-session -A -s main"
+alias t="env TERM=screen-256color tmux new-session -A -s main"
+alias tunnelIpfs="ssh -f -p 22 -L 4001:localhost:4001 -L 5001:localhost:5001 -L 8080:localhost:8080 audacioustux@devlap -N"
